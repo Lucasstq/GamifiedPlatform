e4152b3278bd4a7b15bad0d372d1ba4e480ba677
Lucasstq <fit.luk@outlook.com>
2025-12-20 11:18:46 -0300

    other settings

---
+++ src/main/java/dev/gamified/GamifiedPlatform/GamifiedPlatformApplication.java
@@ -0,0 +1,13 @@
+package dev.gamified.GamifiedPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamifiedPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamifiedPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gamified/GamifiedPlatform/GamifiedPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gamified.GamifiedPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamifiedPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
