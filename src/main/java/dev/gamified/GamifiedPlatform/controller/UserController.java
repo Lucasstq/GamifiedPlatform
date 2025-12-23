dab8a13071c897dd219d1cd9044320c7eb8b43a4
Lucasstq <fit.luk@outlook.com>
2025-12-23 11:37:28 -0300

    feat(controller): add update, get, and delete user endpoints

--- src/main/java/dev/gamified/GamifiedPlatform/controller/UserController.java
+++ src/main/java/dev/gamified/GamifiedPlatform/controller/UserController.java
@@ -1,15 +1,14 @@
 package dev.gamified.GamifiedPlatform.controller;
 
+import dev.gamified.GamifiedPlatform.dtos.request.DeleteUserRequest;
 import dev.gamified.GamifiedPlatform.dtos.request.UserRequest;
 import dev.gamified.GamifiedPlatform.dtos.response.UserResponse;
 import dev.gamified.GamifiedPlatform.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -23,5 +22,24 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(request));
     }
 
+    @PutMapping("/{id}/me")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody UserRequest request) {
+        return ResponseEntity.ok(userService.updateUser(id, request));
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<UserResponse> getUserByUsername(@RequestParam String username) {
+        return ResponseEntity.ok(userService.getUserByUsername(username));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id, @Valid @RequestBody DeleteUserRequest request) {
+        userService.deleteUser(id, request.password());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
