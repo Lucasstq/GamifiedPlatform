b0dffe8b358b163d3c7cf41c37e04c3c9140b4ff
Lucasstq <fit.luk@outlook.com>
2025-12-26 21:50:14 -0300

    refactor(dto): update UserRequest and UserUpdateRequest with role and scopes support

--- src/main/java/dev/gamified/GamifiedPlatform/dtos/request/UserRequest.java
+++ src/main/java/dev/gamified/GamifiedPlatform/dtos/request/UserRequest.java
@@ -1,5 +1,6 @@
 package dev.gamified.GamifiedPlatform.dtos.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -9,6 +10,7 @@ public record UserRequest(
         @NotBlank(message = "Username cannot be null")
         String username,
         @NotBlank(message = "Email cannot be null")
+        @Email(message = "Email should be valid")
         String email,
         @NotBlank(message = "Password cannot be null")
         @Size(min = 8, message = "Password must be at least 8 characters long")
--- src/main/java/dev/gamified/GamifiedPlatform/dtos/request/UserUpdateRequest.java
+++ src/main/java/dev/gamified/GamifiedPlatform/dtos/request/UserUpdateRequest.java
@@ -1,12 +1,16 @@
 package dev.gamified.GamifiedPlatform.dtos.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record UserUpdateRequest(
+        @NotBlank(message = "Username cannot be blank")
         String username,
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Email should be valid")
         String email,
-        String password,
         String avatarUrl
 ) {
 }
