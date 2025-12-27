a9e52a8598c6c9a7e9f1e81ffc498179055d4bdc
Lucasstq <fit.luk@outlook.com>
2025-12-26 21:49:08 -0300

    feat(domain): add role, active status and scopes relationship to User entity

--- src/main/java/dev/gamified/GamifiedPlatform/domain/User.java
+++ src/main/java/dev/gamified/GamifiedPlatform/domain/User.java
@@ -1,11 +1,13 @@
 package dev.gamified.GamifiedPlatform.domain;
 
+import dev.gamified.GamifiedPlatform.enums.Roles;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -31,6 +33,11 @@ public class User {
     @Column(name = "avatar_url")
     private String avatarUrl;
 
+    private Boolean active = true;
+
+    @Enumerated(EnumType.STRING)
+    private Roles role;
+
     @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
@@ -38,6 +45,14 @@ public class User {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_user_scopes",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "scopes_id")
+    )
+    List<Scopes> scopes;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private PlayerCharacter playerCharacter;
