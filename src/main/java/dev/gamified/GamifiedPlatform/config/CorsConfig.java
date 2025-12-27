3f6d6bdab096d9242fd3b010e80d737c6bffd222
Lucasstq <fit.luk@outlook.com>
2025-12-26 21:49:55 -0300

    feat(config): add CORS configuration for cross-origin requests

---
+++ src/main/java/dev/gamified/GamifiedPlatform/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package dev.gamified.GamifiedPlatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+}
